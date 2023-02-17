6315ba82e31b06fed6bc19b49afd6c24ce8887fd
ted-kang1 <tikang@princeton.edu>
2023-02-17 16:20:58 -0500

    initialize

---
+++ Student_tikang.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+public class Student_tikang implements Student {
+  public int[] getApplications(
+      int N,
+      double S,
+      double T,
+      double W,
+      double aptitude,
+      List<Double> schools,
+      List<Double> synergies) {
+    final int[] ret = new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    return ret;
+  }
+}
